badf2bfd6f59a4797b52fd87b4616480cb1ae2f3
sonangrai <shonahangrae@gmail.com>
2025-11-02 19:33:25 +0545

    feat: added primevue UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primeuix/themes/aura";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@primevue/nuxt-module"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
 });
