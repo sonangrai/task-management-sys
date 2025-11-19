df3183b3e7fcf72dba54aeddca09887c520f3cad
sonangrai <shonahangrae@gmail.com>
2025-11-19 19:51:38 +0545

    feat: added a view modal

--- app/components/add-form.vue
+++ app/components/add-form.vue
@@ -1,31 +1,143 @@
 <script lang="ts" setup>
-const wishList = useWishlistStore();
-const newValue = ref("");
+import type { TTask } from "~/type/task";
 
-const handleSubmit = () => {
-  if (newValue.value.trim() != "") {
-    wishList.addWish(newValue.value);
-  }
-  newValue.value = "";
+const props = defineProps<{ task: TTask }>();
+const status = ref([
+  {
+    code: "pending",
+    name: "Pending",
+  },
+  {
+    code: "in_progress",
+    name: "In Progress",
+  },
+  {
+    code: "completed",
+    name: "Completed",
+  },
+  {
+    code: "cancelled",
+    name: "Cancelled",
+  },
+  {
+    code: "on_hold",
+    name: "On Hold",
+  },
+]);
+
+const priority = ref([
+  {
+    code: "urgent",
+    name: "Urgent",
+  },
+  {
+    code: "high",
+    name: "High",
+  },
+  {
+    code: "medium",
+    name: "Medium",
+    color: "text-green-600",
+  },
+  {
+    code: "moderate",
+    name: "Moderate",
+  },
+  {
+    code: "low",
+    name: "Low",
+  },
+]);
+
+const initialData = {
+  ...props.task,
+  due: new Date(props.task.due),
+  status: {
+    code: props.task.status,
+    name: props.task.status[0]?.toUpperCase() + "" + props.task.status.slice(1),
+  },
+  priority: {
+    code: props.task.priority,
+    name:
+      props.task.priority[0]?.toUpperCase() + "" + props.task.priority.slice(1),
+  },
 };
+
+console.log(initialData);
+const data = ref<{
+  title: string;
+  description: string;
+  status: {
+    code: string;
+    name: string;
+  };
+  priority: {
+    code: string;
+    name: string;
+  };
+  due: Date | null;
+}>(initialData);
+
+const handleSubmit = () => {};
 </script>
 
 <template>
-  <form class="mt-2 w-full" @submit.prevent="handleSubmit">
-    <div class="flex gap-2">
-      <input
-        type="text"
-        class="border grow-2 p-2 rounded-md"
-        placeholder="Item of wish"
-        name="newValue"
-        v-model="newValue"
-      />
-      <button
-        type="submit"
-        class="bg-cyan-800 text-white w-[100px] cursor-pointer rounded-md"
-      >
-        Add
-      </button>
+  <form class="flex flex-col md:flex-row gap-6" @submit.prevent="handleSubmit">
+    <div class="grow-2 flex flex-col gap-4">
+      <InputGroup>
+        <InputText v-model="data.title" placeholder="Title" required="true" />
+      </InputGroup>
+
+      <InputGroup>
+        <Editor
+          v-model="data.description"
+          class="w-full h-[500px] md:h-[70vh]"
+          required
+        />
+      </InputGroup>
+    </div>
+
+    <div class="w-full md:w-[300px] flex flex-col gap-4">
+      <InputGroup>
+        <Select
+          v-model="data.status"
+          :options="status"
+          optionLabel="name"
+          placeholder="Select a Status"
+          class="w-full"
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <Select
+          v-model="data.priority"
+          :options="priority"
+          optionLabel="name"
+          placeholder="Select a priority"
+          class="w-full"
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <DatePicker
+          id="datepicker-12h"
+          v-model="data.due"
+          showTime
+          hourFormat="12"
+          placeholder="Due"
+          fluid
+          required
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <Button
+          type="submit"
+          severity="secondary"
+          icon="pi pi-pencil"
+          class="w-full"
+          label="Update"
+      /></InputGroup>
     </div>
   </form>
 </template>
--- app/components/task-list.vue
+++ app/components/task-list.vue
@@ -33,6 +33,10 @@ const TASK_STATUS = [
     color: "text-yellow-600",
   },
 ];
+
+const dialogVisible = ref<boolean>(false);
+const chosenTask = ref<TTask | null>(null);
+
 const config = useRuntimeConfig();
 const supabase = useSupabaseClient();
 const {
@@ -68,37 +72,57 @@ const buildData = tasks
       };
     })
   : [];
+
+const openModal = (data: TTask) => {
+  dialogVisible.value = true;
+  chosenTask.value = data;
+};
 </script>
 
 <template>
-  <div>
-    <div v-if="status === 'pending'">Loading ...</div>
-    <div class="grid gap-6 grid-cols-[repeat(5,1fr)] my-8" v-else>
-      <div v-for="state in buildData" :key="state.id">
-        <div class="border border-green-700">
-          <div class="p-2 border-b">
-            <h2 class="text-md text-center font-bold" :class="state.color">
-              {{ state.label }}
-            </h2>
-          </div>
+  <div v-if="status === 'pending'">Loading ...</div>
+  <div class="grid gap-6 grid-cols-[repeat(5,1fr)] my-8" v-else>
+    <div v-for="state in buildData" :key="state.id">
+      <div class="border border-green-700">
+        <div class="p-2 border-b">
+          <h2 class="text-md text-center font-bold" :class="state.color">
+            {{ state.label }}
+          </h2>
+        </div>
 
-          <div class="p-2 flex flex-col gap-2 max-h-[70vh] overflow-auto">
-            <template v-if="state.data.length > 0">
-              <card-task
-                v-for="task in state.data"
-                :key="task.id"
-                :task="task"
-              />
-            </template>
-            <div
-              v-else
-              class="text-sm text-gray-400 font-bold h-20 flex justify-center items-center"
-            >
-              No Task
-            </div>
+        <div class="p-2 flex flex-col gap-2 max-h-[70vh] overflow-auto">
+          <template v-if="state.data.length > 0">
+            <card-task
+              v-for="task in state.data"
+              :key="task.id"
+              :task="task"
+              @click="openModal(task)"
+            />
+          </template>
+          <div
+            v-else
+            class="text-sm text-gray-400 font-bold h-20 flex justify-center items-center"
+          >
+            No Task
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <Dialog
+    v-model:visible="dialogVisible"
+    modal
+    header="Edit Profile"
+    :style="{ width: '80rem' }"
+  >
+    <template #header>
+      <div class="inline-flex items-center justify-center gap-2">
+        <span class="font-bold whitespace-nowrap">{{ chosenTask?.title }}</span>
+      </div>
+    </template>
+    <template v-if="chosenTask">
+      <add-form :task="chosenTask" />
+    </template>
+  </Dialog>
 </template>
