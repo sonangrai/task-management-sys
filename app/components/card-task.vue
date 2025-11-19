918b9a71964d59fa8ca7b29e59f382ad680a9085
sonangrai <shonahangrae@gmail.com>
2025-11-19 19:22:26 +0545

    feat: looped with proper fitlered data

---
+++ app/components/card-task.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { TTask } from "~/type/task";
+
+const props = defineProps<{ task: TTask }>();
+</script>
+
+<template>
+  <div class="border border-green-700 cursor-pointer p-2">
+    <h2 class="text-md font-medium">
+      {{ props.task.title }}
+    </h2>
+
+    <p class="font-sm italic">Due: {{ props.task.due.slice(0, 10) }}</p>
+
+    <Tag severity="secondary" value="Secondary" rounded>{{
+      props.task.priority
+    }}</Tag>
+  </div>
+</template>
--- app/components/task-list.vue
+++ app/components/task-list.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { TTask, TResponse } from "~/type/task";
+
 const TASK_STATUS = [
   {
     id: 1,
@@ -31,22 +33,70 @@ const TASK_STATUS = [
     color: "text-yellow-600",
   },
 ];
+const config = useRuntimeConfig();
+const supabase = useSupabaseClient();
+const {
+  data: { session },
+} = await supabase.auth.getSession();
+
+async function getTasksAPI() {
+  const { data } = await $fetch<TResponse<TTask[]>>(
+    `${config.public.backendUrl}/api/task`,
+    {
+      headers: {
+        Authorization: `Bearer ${session?.access_token}`,
+      },
+    }
+  );
+
+  return data;
+}
+
+const { data, status } = await useAsyncData<TTask[]>("tasks", () =>
+  getTasksAPI()
+);
+
+const tasks = data?.value || null;
+
+const buildData = tasks
+  ? TASK_STATUS.map((stat) => {
+      return {
+        ...stat,
+        data: tasks.filter((task) => {
+          return task.status == stat.key ? task : undefined;
+        }),
+      };
+    })
+  : [];
 </script>
 
 <template>
-  <div class="grid gap-6 grid-cols-[repeat(5,1fr)] my-8">
-    <div v-for="state in TASK_STATUS" :key="state.id">
-      <div class="border border-green-700">
-        <div class="p-2 border-b">
-          <h2 class="text-md text-center font-bold" :class="state.color">
-            {{ state.label }}
-          </h2>
-        </div>
+  <div>
+    <div v-if="status === 'pending'">Loading ...</div>
+    <div class="grid gap-6 grid-cols-[repeat(5,1fr)] my-8" v-else>
+      <div v-for="state in buildData" :key="state.id">
+        <div class="border border-green-700">
+          <div class="p-2 border-b">
+            <h2 class="text-md text-center font-bold" :class="state.color">
+              {{ state.label }}
+            </h2>
+          </div>
 
-        <div class="p-2">
-          <ul>
-            <li>HEHE</li>
-          </ul>
+          <div class="p-2 flex flex-col gap-2">
+            <template v-if="state.data.length > 0">
+              <card-task
+                v-for="task in state.data"
+                :key="task.id"
+                :task="task"
+              />
+            </template>
+            <div
+              v-else
+              class="text-sm text-gray-400 font-bold h-20 flex justify-center items-center"
+            >
+              No Task
+            </div>
+          </div>
         </div>
       </div>
     </div>
